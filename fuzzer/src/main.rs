03a4aee77681ece9367bd09caf96d9201ccb4dc1
galtashma <galtashma@gmail.com>
2021-05-13 11:23:29 +0300

    playing around with small changes

--- fuzzer/src/main.rs
+++ fuzzer/src/main.rs
@@ -1,37 +1,41 @@
+use libafl::{
+    bolts::shmem::{ShMem, ShMemProvider, StdShMemProvider},
+    corpus::IndexesLenTimeMinimizerCorpusScheduler,
+    feedbacks::{MapFeedback, MaxReducer, TimeFeedback, TimeoutFeedback},
+    utils::RomuTrioRand,
+};
 use std::path::PathBuf;
-use libafl::bolts::shmem::{ShMemProvider, StdShMemProvider, ShMem};
 
 use libafl::{
     bolts::tuples::tuple_list,
     corpus::{InMemoryCorpus, OnDiskCorpus, QueueCorpusScheduler},
     events::SimpleEventManager,
-    feedbacks::{CrashFeedback, MaxMapFeedback},
+    feedbacks::{CrashFeedback, FeedbacksTuple, MaxMapFeedback},
     fuzzer::{Fuzzer, StdFuzzer},
-    generators::RandPrintablesGenerator,
+    inputs::BytesInput,
     mutators::scheduled::{havoc_mutations, StdScheduledMutator},
+    mutators::token_mutations::Tokens,
+    observers::TimeObserver,
     stages::mutational::StdMutationalStage,
     state::State,
     stats::SimpleStats,
     utils::{current_nanos, StdRand},
 };
+use log::debug;
 
 mod qemu;
 
-use qemu::{
-    forkserver::ForkserverExecutor,
-    observer::SharedMemObserver,
-};
-
+use qemu::{forkserver::ForkserverExecutor, observer::SharedMemObserver};
 
 const MAP_SIZE: usize = 1 << 16;
 
 // TODO
-// 1. observe shared memory - using StdMapObserver::new_from_ptr
-// 2. run full harness in snapshots
-// 3. update inputs between iterations
-// 4. generator to use pre-made corpus using 
+// ?. observe shared memory - using StdMapObserver::new_from_ptr
+// ?. run full harness in snapshots
+// V update inputs between iterations
+// 4. generator to use pre-made corpus using
 //      state.load_initial_inputs(executor, manager,scheduler, corpus_dir)
-// 5. FuzzerExecutor to accept env, args and bin (should it take also shared mem? or should it
+// V. FuzzerExecutor to accept env, args and bin (should it take also shared mem? or should it
 //    create it?)
 
 pub fn main() {
@@ -44,18 +48,23 @@ pub fn main() {
     // such as the notification of the addition of a new item to the corpus
     let mut mgr = SimpleEventManager::new(stats);
 
-    let observer: SharedMemObserver<u8> = SharedMemObserver::new("coverage", "__AFL_SHM_ID", MAP_SIZE);
+    let coverage_observer: SharedMemObserver<u8> =
+        SharedMemObserver::new("coverage", "__AFL_SHM_ID", MAP_SIZE);
 
-    let feedback = MaxMapFeedback::new_with_observer(&observer);
+    let coverage_feedback =
+        MaxMapFeedback::new_with_observer_track(&coverage_observer, true, false);
+
+    // let temp_corpus : InMemoryCorpus<BytesInput> = InMemoryCorpus::new();
+    let temp_corpus = OnDiskCorpus::new(PathBuf::from("./queue")).unwrap();
 
     // create a State from scratch
     let mut state = State::new(
         // RNG
         StdRand::with_seed(current_nanos()),
         // Corpus that will be evolved, we keep it in memory for performance
-        OnDiskCorpus::new(PathBuf::from("./queue")).unwrap(),
+        temp_corpus,
         // Feedbacks to rate the interestingness of an input
-        tuple_list!(feedback),
+        tuple_list!(coverage_feedback),
         // Corpus in which we store solutions (crashes in this example),
         // on disk so the user can get them after stopping the fuzzer
         OnDiskCorpus::new(PathBuf::from("./crashes")).unwrap(),
@@ -71,25 +80,39 @@ pub fn main() {
     let mut fuzzer = StdFuzzer::new(tuple_list!(stage));
 
     // A queue policy to get testcasess from the corpus
-    let scheduler = QueueCorpusScheduler::new();
+    // let scheduler = QueueCorpusScheduler::new();
+    let minimizer_sched = IndexesLenTimeMinimizerCorpusScheduler::new(QueueCorpusScheduler::new());
 
     let mut executor = ForkserverExecutor::new(
-        "/fuzz/bin/harness", 
+        "/fuzz/bin/harness",
         vec!["@@"],
-        tuple_list!(observer)
-    ).expect("Failed to create the Executor".into());
-        
+        tuple_list!(coverage_observer),
+    )
+    .expect("Failed to create the Executor".into());
 
-    // Generator of printable bytearrays of max size 32
-    let mut generator = RandPrintablesGenerator::new(32);
+    let corpuses = vec![PathBuf::from("./corpus")];
 
-    // Generate 8 initial inputs
+    // this should run all files in corpus folder and record coverage for them
     state
-        .generate_initial_inputs(&mut executor, &mut generator, &mut mgr, &scheduler, 8)
-        .expect("Failed to generate the initial corpus".into());
+        .load_initial_inputs(
+            &mut executor,
+            &mut mgr,
+            &minimizer_sched,
+            corpuses.as_slice(),
+        )
+        .expect("Failed to load initial corpus");
+
+    debug!("[+] done loading initial corpus");
+
+    // let testcase = temp_corpus.get(0);
+    //
+    // scheduler ->
+    //  executor
+
+    // fuzzer.fuzz_one(&mut state, &mut executor, &mut mgr, &scheduler)
+    //     .expect("Error running fuzzer once");
 
     fuzzer
-        .fuzz_loop(&mut state, &mut executor, &mut mgr, &scheduler)
+        .fuzz_loop(&mut state, &mut executor, &mut mgr, &minimizer_sched)
         .expect("Error in the fuzzing loop".into());
 }
-
